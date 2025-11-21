de49be8d65a9e9ecd70aaaf67a83246f07f62e0e
burce <burcev-egor@mail.ru>
2025-11-21 12:23:45 +0500

    service impl

--- internal/service/pr_service/pr_service.go
+++ internal/service/pr_service/pr_service.go
@@ -1,1 +1,46 @@
 package pr_service
+
+import (
+	"PRService/internal/model"
+	"PRService/internal/service"
+)
+
+var _ service.Service = (*Service)(nil)
+
+type Service struct {
+}
+
+func (s Service) CreateTeam(team *model.Team) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) GetTeam(teamName string) (*model.Team, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) SetUserIsActive(id model.UserID) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) GetReviewersPRs(id model.UserID) ([]model.PullRequest, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) CreatePullRequest(request model.PullRequest) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) MergePullRequest(id model.PullRequestID) (*model.PullRequest, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) ReassignPullRequestReviewer(id model.PullRequestID, id2 model.UserID) error {
+	//TODO implement me
+	panic("implement me")
+}
