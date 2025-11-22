2649d9499347d3ce902dbc4254a8026c7920f452
burce <burcev-egor@mail.ru>
2025-11-22 21:13:13 +0500

    api response fix

--- internal/router/get_reviews.go
+++ internal/router/get_reviews.go
@@ -17,8 +17,26 @@ func (r *Router) getReview(c fiber.Ctx) error {
 		return r.ProcessError(c, err)
 	}
 
-	return c.Status(fiber.StatusOK).JSON(map[string]any{
+	result := make([]PullRequestDto, 0, len(pullRequests))
+	for i := range pullRequests {
+		result = append(result, PullRequestDto{
+			PullRequestID:   pullRequests[i].ID,
+			PullRequestName: pullRequests[i].Name,
+			AuthorID:        pullRequests[i].AuthorID,
+			Status:          pullRequests[i].Status,
+		})
+	}
+	response := map[string]any{
 		"user_id":      userID,
-		"pullRequests": pullRequests,
-	})
+		"pullRequests": result,
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response)
+}
+
+type PullRequestDto struct {
+	PullRequestID   model.PullRequestID     `json:"pull_request_id"`
+	PullRequestName string                  `json:"pull_request_name"`
+	AuthorID        model.UserID            `json:"author_id"`
+	Status          model.PullRequestStatus `json:"status"`
 }
