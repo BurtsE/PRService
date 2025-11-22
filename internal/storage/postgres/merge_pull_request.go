c927d54788259a1625b0e4856dc3369670cd70d7
burce <burcev-egor@mail.ru>
2025-11-22 17:27:31 +0500

    repo implemented

--- internal/storage/postgres/merge_pull_request.go
+++ internal/storage/postgres/merge_pull_request.go
@@ -6,6 +6,25 @@ import (
 )
 
 func (r *Repository) MergePullRequest(ctx context.Context, id model.PullRequestID) (*model.PullRequest, error) {
-	//TODO implement me
-	panic("implement me")
+	query := `
+		UPDATE pull_requests
+		SET status = $2 merged_at = NOW()
+		WHERE id = $1
+		RETURNING id, name, author_id, status, created_at, merged_at
+	`
+	var pr model.PullRequest
+	err := r.c.QueryRow(ctx, query, id, model.PullRequestStatusMerged).Scan(
+		&pr.Id,
+		&pr.Name,
+		&pr.AuthorId,
+		&pr.Status,
+		&pr.CreatedAt,
+		&pr.MergedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pr, nil
+
 }
