92ae333887201db15e9bd88a4114c763dcb775dd
burce <burcev-egor@mail.ru>
2025-11-22 17:48:09 +0500

    naming change

--- internal/storage/postgres/merge_pull_request.go
+++ internal/storage/postgres/merge_pull_request.go
@@ -5,7 +5,7 @@ import (
 	"context"
 )
 
-func (r *Repository) MergePullRequest(ctx context.Context, id model.PullRequestID) (*model.PullRequest, error) {
+func (r *Repository) MergePullRequest(ctx context.Context, requestID model.PullRequestID) (*model.PullRequest, error) {
 	query := `
 		UPDATE pull_requests
 		SET status = $2, merged_at = NOW()
@@ -20,7 +20,7 @@ func (r *Repository) MergePullRequest(ctx context.Context, id model.PullRequestI
 	defer func() { _ = tx.Rollback(ctx) }()
 
 	var pr model.PullRequest
-	err = tx.QueryRow(ctx, query, id, model.PullRequestStatusMerged).Scan(
+	err = tx.QueryRow(ctx, query, requestID, model.PullRequestStatusMerged).Scan(
 		&pr.Id,
 		&pr.Name,
 		&pr.AuthorId,
@@ -37,19 +37,19 @@ func (r *Repository) MergePullRequest(ctx context.Context, id model.PullRequestI
 		FROM pull_request_reviewers
 		WHERE pull_request_id = $1
 	`
-	rows, err := tx.Query(ctx, query, id)
+	rows, err := tx.Query(ctx, query, requestID)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var id model.UserID
-		err = rows.Scan(&id)
+		var userID model.UserID
+		err = rows.Scan(&userID)
 		if err != nil {
 			return nil, err
 		}
-		pr.Reviewers = append(pr.Reviewers, id)
+		pr.Reviewers = append(pr.Reviewers, userID)
 	}
 
 	if err = tx.Commit(ctx); err != nil {
