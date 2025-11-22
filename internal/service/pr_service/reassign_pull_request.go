bc2e6cc407318397833e99b6d5d932a35925b4c7
burce <burcev-egor@mail.ru>
2025-11-22 18:09:55 +0500

    service impl added

--- internal/service/pr_service/reassign_pull_request.go
+++ internal/service/pr_service/reassign_pull_request.go
@@ -2,11 +2,25 @@ package pr_service
 
 import (
 	"PRService/internal/model"
+	"PRService/internal/service"
 	"context"
 )
 
 func (s *Service) ReassignPullRequestReviewer(ctx context.Context,
-	pullRequestID model.PullRequestID, userID model.UserID) error {
-	//TODO implement me
-	panic("implement me")
+	pullRequestID model.PullRequestID, userID model.UserID) (*model.PullRequest, error) {
+
+	exists, err := s.storage.PullRequestExists(ctx, pullRequestID)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		return nil, service.ErrResourceNotFound
+	}
+
+	pullRequest, err := s.storage.ReassignPullRequestReviewer(ctx, pullRequestID, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	return pullRequest, nil
 }
